392e0f97a9074d02eb611b0ca8f55f402781846a
sushmasree2004 <sushmasree2004@gmail.com>
2026-02-18 23:57:40 +0530

    uc4 feature added

--- Main.java
+++ Main.java
@@ -1,21 +1,36 @@
 /*
 
 @author : B.Sushma Sree
-@version: 2
+@version: 4
 
 */
+
 
 class Main
 {
     public static void main(String args[])
-   {
-         System.out.println("   ****    "+"  "+"   ****    "+"  *********"+"    ******");
-         System.out.println("  ***  *** "+"  "+"  ***  *** "+"  *********"+"   **     ");
-         System.out.println(" **     ** "+"  "+" **     ** "+"  *      **"+"  **      ");
-         System.out.println(" **     ** "+"  "+" **     ** "+"  *********"+"    ****  ");
-         System.out.println(" **     ** "+"  "+" **     ** "+"  *********"+"        **");
-         System.out.println("  ***  *** "+"  "+"  ***  *** "+"  **       "+"        **");
-         System.out.println("   ****    "+"  "+"   ****    "+"  **       "+"   ****** ");
-    }
+    {
+         String[] lines=new String[7];
+            
+
+         lines[0]=String.join(" ","    ****    ","   ****    ","  *********","    ******");
+         lines[1]=String.join(" ","  ***  ***  ","  ***   ***","  *********","   **     ");
+         lines[2]=String.join(" ","  **     ** ","**      ** ","  *      **","  **      ");
+         lines[3]=String.join(" ","  **     ** ","**      ** ","  *********","    ****  ");
+         lines[4]=String.join(" ","  **     ** ","**      ** ","  *********","        **");
+         lines[5]=String.join(" ","  ***  ***  ","  ***  *** ","  **       ","        **");
+         lines[6]=String.join(" ","    ****    ","   ****    ","  **       ","   ****** ");
+
+         for(String line:lines)
+         { 
+           System.out.println(line);
+         }
+
+
+
+     }
 
 }
+
+
+
