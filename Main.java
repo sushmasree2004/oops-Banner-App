995f8709e155a964b586fa24c4829cddd5da0dd7
sushmasree2004 <sushmasree2004@gmail.com>
2026-02-19 10:25:09 +0530

    uc6 feature added

--- Main.java
+++ Main.java
@@ -1,25 +1,98 @@
 /*
 
 @author : B.Sushma Sree
-@version: 5
+@version: 6
 
 */
 
 
 class Main
 {
+    public static String[] getPatternO(){
+        return new String[]
+            {
+                 "    *****     ",
+                 "  ***    ***  ",
+                 " **        ** ",
+                 " **        ** ",
+                 " **        ** ",
+                 "  ***    ***  ",
+                 "    *****     "
+            };
+    }
+
+
+
+    public static String[] getPatternP(){
+        return new String[]
+            {
+                 " ************* ",
+                 " ************* ",
+                 " **        *** ",
+                 " ************* ",
+                 " ************* ",
+                 " **            ",
+                 " **            ",
+				 " **            "
+            };
+    }
+	
+	public static String[]  getPatternS(){
+		
+		return new String[]
+		{
+			
+			"    ******  ",
+			"   **       ",
+			"  **        ",
+			"    ****    ",
+			"        **  ",
+			"         ** ",
+			"   ******   "
+				
+			
+		};
+		
+		
+	}
+	
+	
+	
     public static void main(String args[])
     {
-         String[] names={"7-Eleven","Slurpee","GSC","squad","Intern","Banglore","Yelhanka"};   
-
-         for(String line: names)
-         { 
-           System.out.println(line);
-         }
+        String []o1=getPatternO();
+		String []o2=getPatternO();
+		String [] p=getPatternP();
+		String [] s=getPatternS();
+		
+		
+		for(int i=0;i<o1.length;i++)
+		{
+			
+			System.out.println(o1[i]);
+		}
+		
+		for(int i=0;i<o2.length;i++)
+		{
+			
+			System.out.println(o2[i]);
+		}
+		
+		
+		for(int i=0;i<p.length;i++)
+		{
+			
+			System.out.println(p[i]);
+		}
+		
+		for(int i=0;i<s.length;i++)
+		{
+			
+			System.out.println(s[i]);
+		}
 
 
 
      }
 
 }
-
